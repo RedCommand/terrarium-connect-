2ca6201f7bd24242e01c894a73aa041c05611f71
RedCommand <maxvidal1612@gmail.com>
2021-03-21 22:21:32 +0100

    add temp fonction to arduino, it will be the same thing for the hot spot

--- arduino/main/src/main.cpp
+++ arduino/main/src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-float temp_zone_froide;                                         // création de la variable pour la température de la zone froide
-Servo ServoTrappe;                                              // création d'un servo
+float temp_zone_froide;                                                         // création de la variable pour la température de la zone froide
+Servo ServoTrappe;                                                              // création d'un servo
 
 void setup() {
   // put your setup code here, to run once:
 
-  ServoTrappe.attach(3);                                        // initialisation de la broche 3 pour le servo moteur (pour la trappe)
+  ServoTrappe.attach(3);                                                        // initialisation de la broche 3 pour le servo moteur (pour la trappe)
 }
 
 void loop() {
@@ -24,4 +24,12 @@ void trappe(int angle = 90) {
   ServoTrappe.write(angle);
 }
 
-void
+void get_temp_zone_froide() {
+  // création d'une fonction permetant de lire la température de la zone froide
+  // exemple d'utilisation de la fonction : "float maTemperature = get_temp_zone_froide()"
+  int temp = analogRead(A1);                                                    // lis la valeur analogique sur la broche A1 (le capteur de température de la zone froide)
+  temp = map(temp, 0, 1023, 0, 5000);                                           // convertie la valeur analogique en mV
+  float pas = 0.01;                                                             // exemple pour un pas de 0.01°C/mV
+  float temperature = temp * pas;                                               // convertie la variable "temp" en °C (grâce au pas)
+  return (temperature);                                                         // renvoie la température
+}
