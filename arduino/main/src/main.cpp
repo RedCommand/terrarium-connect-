baee6ea784aa7a2022ed738820f7b29d3bc2f25e
RedCommand <maxvidal1612@gmail.com>
2021-03-21 22:31:02 +0100

    correction des fautes d'orthographe. Merci Florestan :)

--- arduino/main/src/main.cpp
+++ arduino/main/src/main.cpp
@@ -17,19 +17,19 @@ void loop() {
 
 
 void trappe(int angle = 90) {
-  // création d'une fonction permetant de gérer la trappe (possibilitée de définir l'angle d'ouverture, sinon l'angle par défault sera 90°)
+  // création d'une fonction permettant de gérer la trappe (possibilitée de définir l'angle d'ouverture, sinon l'angle par défault sera 90°)
   // exemple d'utilisation de la fonction : "trappe()" pour ouvrir la trappe à l'angle par default. Ou bien : "trappe(35)" pour ouvrir la trappe à 35°.
-  // ATTENTION : la variable "angle" dois etre comprise entre 0 et 180°
+  // ATTENTION : la variable "angle" doit être comprise entre 0 et 180
 
   ServoTrappe.write(angle);
 }
 
 void get_temp_zone_froide() {
-  // création d'une fonction permetant de lire la température de la zone froide
+  // création d'une fonction permettant de lire la température de la zone froide
   // exemple d'utilisation de la fonction : "float maTemperature = get_temp_zone_froide()"
-  int temp = analogRead(A1);                                                    // lis la valeur analogique sur la broche A1 (le capteur de température de la zone froide)
-  temp = map(temp, 0, 1023, 0, 5000);                                           // convertie la valeur analogique en mV
+  int temp = analogRead(A1);                                                    // lit la valeur analogique sur la broche A1 (le capteur de température de la zone froide)
+  temp = map(temp, 0, 1023, 0, 5000);                                           // convertit la valeur analogique en mV
   float pas = 0.01;                                                             // exemple pour un pas de 0.01°C/mV
-  float temperature = temp * pas;                                               // convertie la variable "temp" en °C (grâce au pas)
+  float temperature = temp * pas;                                               // convertit la variable "temp" en °C (grâce au pas)
   return (temperature);                                                         // renvoie la température
 }
