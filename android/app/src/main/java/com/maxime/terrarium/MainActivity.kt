77f104c0ffad3a447f2211208024b86a7ef8c883
RedCommand <maxvidal1612@gmail.com>
2021-03-25 11:45:45 +0100

    create login activity

--- android/app/src/main/java/com/maxime/terrarium/MainActivity.kt
+++ android/app/src/main/java/com/maxime/terrarium/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.maxime.terrarium
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.maxime.terrarium.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun login_button(button: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
