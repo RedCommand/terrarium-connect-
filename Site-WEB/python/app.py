a60f4ce8efeffea208458bde55cbf08bff13fcfb
RedCommand <maxvidal1612@gmail.com>
2021-04-21 17:51:30 +0200

    finishing server

--- Site-WEB/python/app.py
+++ Site-WEB/python/app.py
@@ -3,6 +3,8 @@ import mysql.connector
 from flask_restx import Resource, Api
 import base64
 
+app = Flask(__name__)
+api = Api(app)
 
 def connect_database(id_devices=None, devices_type=None, devices_exist=True):
     mydb = mysql.connector.connect(
@@ -18,49 +20,38 @@ def connect_database(id_devices=None, devices_type=None, devices_exist=True):
             mycursor.execute("INSERT INTO devices (id, type, temp_zone_chaude, current_temp_zone_chaude, temp_zone_froide, current_temp_zone_froide, angle_trappe, humidity, current_humidity) SELECT {}, '{}', 34, 34, 24, 24, 42, 42, 42 WHERE NOT EXISTS (SELECT * FROM devices WHERE id={});".format(id_devices, devices_type, id_devices))
             mydb.commit()
             print("created new devices")
-
-
     return mydb, mycursor
 
-
-class create_dict(dict):
-
-    # __init__ function
-    def __init__(self):
-        self = dict()
-
-    # Function to add key:value
-    def add(self, key, value):
-        self[key] = value
-
-
-
-
-
-
-
-
-app = Flask(__name__)
-
-api = Api(app)
 
 
 
 @api.route('/client/<client_token>/<fonction>')
 class client(Resource):
-
-
-
     def get(self, client_token, fonction):
-
         result = None
 
+
         def user_exist():
-            mycursor.execute("SELECT user FROM `users` WHERE user='{}'".format(login))
+            mycursor.execute("SELECT user FROM `users` WHERE user='{}' AND password='{}'".format(login, password))
             if "{}".format(mycursor.fetchall()) != "[]":
                 return True
             else:
                 return False
+
+        def list_user_devices():
+            if user_exist():
+                user_devices_list = []
+                mycursor.execute("SELECT `devices_list` FROM users WHERE user='{}'".format(login))
+                output= mycursor.fetchall()
+                output = output[0]
+                output = output[0]
+                output = json.loads(output)
+                for i in output.get("id"):
+                    user_devices_list.append(i)
+                print("user_devices_list = ", user_devices_list)
+                return user_devices_list
+            else:
+                return "user or password incorrect"
 
 
         def register():
@@ -72,10 +63,39 @@ class client(Resource):
 
         def list_devices():
             if user_exist():
-                mycursor.execute("SELECT devices_list FROM `users` WHERE user='{}' AND password='{}'".format(login, password))
-                return mycursor.fetchall()
+                mycursor.execute("SELECT `devices_list` FROM users WHERE user='{}'".format(login))
+                output= mycursor.fetchall()
+                output = output[0]
+                output = output[0]
+                output = json.loads(output)
+                result = []
+                for i in output.get("id"):
+                    device_id = i
+                    mycursor.execute("SELECT `type` FROM `devices` WHERE `id`={}".format(device_id))
+                    device_type = mycursor.fetchone()
+                    device_type = device_type[0]
+                    print(device_type)
+                    result.append((device_id, device_type))
+                return result
             else:
-                return "user does not exist"
+                return "user or password incorrect"
+
+        def get_data():
+            if user_exist():
+                id_device = request.args.get("id_device")
+                id_device = int(id_device)
+                user_devices = list_user_devices()
+                print("id_device = ", id_device)
+                if id_device in user_devices:
+                    mycursor.execute("SELECT * FROM `devices` WHERE id={}".format(id_device))
+                    output = mycursor.fetchone()
+                    mydict = {"id":output[0],"type":output[1],"temp_zone_chaude":output[2],"current_temp_zone_chaude":output[3], "temp_zone_froide":output[4], "current_temp_zone_froide":output[5], "angle_trappe":output[6], "humidity":output[7], "current_humidity":output[8]}
+                    return mydict
+                else:
+                    return "wrong device"
+            else:
+                return "user or password incorrect"
+
 
 
         mydb, mycursor = connect_database()
@@ -84,22 +104,17 @@ class client(Resource):
             login, password = base64.b64decode(client_token).decode("UTF-8").split('::')
         except:
             result = "invalid token"
-
         print("login = ", login, "\npassword = ", password)
+
 
         if fonction == "register":
             result = register()
-
         elif fonction == "list_devices":
             result = list_devices()
-
+        elif fonction == "get_data":
+            result = get_data()
         else:
             result =  "bad request"
-
-
-
-
-
 
 
         mycursor.close()
@@ -110,8 +125,115 @@ class client(Resource):
 
 
 
-    def post(self):
-        pass
+    def post(self, client_token, fonction):
+
+        def user_exist():
+            mycursor.execute("SELECT user FROM `users` WHERE user='{}' AND password='{}'".format(login, password))
+            if "{}".format(mycursor.fetchall()) != "[]":
+                return True
+            else:
+                return False
+
+        def list_user_devices():
+            if user_exist():
+                user_devices_list = []
+                mycursor.execute("SELECT `devices_list` FROM users WHERE user='{}'".format(login))
+                output= mycursor.fetchall()
+                output = output[0]
+                output = output[0]
+                output = json.loads(output)
+                for i in output.get("id"):
+                    user_devices_list.append(i)
+                print("user_devices_list = ", user_devices_list)
+                return user_devices_list
+            else:
+                return "user or password incorrect"
+
+
+        def update_data():
+            if user_exist():
+                nb_request = 0
+                sql_request = "UPDATE devices SET"
+                try:
+                    temp_zone_chaude = request.form['temp_zone_chaude']
+                    if nb_request > 0:
+                        sql_request += ","
+                    sql_request += " temp_zone_chaude="
+                    sql_request += temp_zone_chaude
+                    nb_request += 1
+                except:
+                    pass
+                try:
+                    temp_zone_froide = request.form['temp_zone_froide']
+                    if nb_request > 0:
+                        sql_request += ","
+                    sql_request += " temp_zone_froide="
+                    sql_request += temp_zone_froide
+                    nb_request += 1
+                except:
+                    pass
+                try:
+                    angle_trappe = request.form['angle_trappe']
+                    if nb_request > 0:
+                        sql_request += ","
+                    sql_request += " angle_trappe="
+                    sql_request += angle_trappe
+                    nb_request += 1
+                except:
+                    pass
+                try:
+                    humidity = request.form['humidity']
+                    if nb_request > 0:
+                        sql_request += ","
+                    sql_request += " humidity="
+                    sql_request += humidity
+                    nb_request += 1
+                except:
+                    pass
+                try:
+                    id_device = request.form['id_device']
+                    id_device = int(id_device)
+                    sql_request += " WHERE id={}".format(id_device)
+                except:
+                    nb_request = 0
+
+                if nb_request > 0:
+                    user_devices = list_user_devices()
+                    print("id_device = ", id_device)
+                    if id_device in user_devices:
+                        print("nb arg = ", nb_request)
+                        print("sql request = ", sql_request)
+                        mycursor.execute(sql_request)
+                        mydb.commit()
+                    else:
+                        return "wrong device"
+                else:
+                    return "missing arg"
+            else:
+                return "user or password incorrect"
+
+
+
+        mydb, mycursor = connect_database()
+
+        try:
+            login, password = base64.b64decode(client_token).decode("UTF-8").split('::')
+        except:
+            result = "invalid token"
+        print("login = ", login, "\npassword = ", password)
+
+
+        if fonction == "update_data":
+            result = update_data()
+        else:
+            result =  "bad request"
+
+
+        mycursor.close()
+        mydb.close()
+
+        print("result")
+        return result
 
 
 
@@ -126,6 +248,7 @@ def arduino():
 
     id_devices, devices_type = base64.b64decode(token_devices).decode("UTF-8").split('::')
 
+
     mydb, mycursor = connect_database(id_devices, devices_type, False)
 
 
@@ -136,34 +259,13 @@ def arduino():
     mycursor.execute("SELECT `temp_zone_chaude`, `temp_zone_froide`, `angle_trappe`, `humidity` FROM `devices` WHERE id={}".format(id_devices))
 
     var = ""
-
     for x in mycursor:
         var = var + "{}".format(x)
 
     mycursor.close()
     mydb.close()
+
     return var
-
-
-@app.route('/test')
-def test():
-
-    mydb, mycursor = connect_database()
-
-    mydict = create_dict()
-    mycursor.execute("SELECT uesr FROM devices")
-    result = mycursor.fetchall()
-
-    print(result)
-
-    for row in result:
-        mydict.add(row[0],({"status":row[1],"temp_zone_chaude":row[2],"current_temp_zone_chaude":row[3], "temp_zone_froide":row[4], "current_temp_zone_froide":row[5], "angle_trappe":row[6], "humidity":row[7], "current_humidity":row[8]}))
-
-    myjson = json.dumps(mydict, indent=2, sort_keys=True)
-    mycursor.close()
-    mydb.close()
-    return myjson
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000, debug=True)
